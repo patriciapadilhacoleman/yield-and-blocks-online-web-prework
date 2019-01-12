bba3deb7bf46cc7c56d5348d893a386bb3c651d4
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-12 21:35:21 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,10 +6,9 @@ def hello_t(array)
     i = i + 1
   end
   array
-  
 end
 
-# call your method here!
+
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
